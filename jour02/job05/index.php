677d182a5ccec0278b1a6d46f2aa5e1526d99bb8
mohamed-azzouz <mohamed.azzouz@laplateforme.io>
2019-11-05 10:30:49 +0100

    Nombre Premier

---
+++ jour02/job05/index.php
@@ -0,0 +1,33 @@
+<?php
+
+$nb = 1;
+$nb_max = 1000;
+		
+while ($nb <= $nb_max) {
+  if ($nb == 1) {
+  
+  $nb++;
+  continue;
+  }
+$a = 0;
+  for ($i=2; $i<$nb; $i++){
+  
+    if (is_int($nb/$i)){
+    $a++;
+    
+    break;
+    }
+  }
+if ($a == 0) echo $nb.'= Est premier<br>';
+$nb++;
+}
+
+?>
+
+
+
+
+
+
+
+
