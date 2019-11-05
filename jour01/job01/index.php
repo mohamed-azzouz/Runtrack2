23d8d7a84049fa176b45a0661e08a270b4ab6449
mohamed-azzouz <mohamed.azzouz@laplateforme.io>
2019-11-05 10:26:40 +0100

    Concatener variable

---
+++ jour01/job01/index.php
@@ -0,0 +1,15 @@
+<?php
+	$str = "Hello LaPlateforme";
+	echo $str;
+	$str2 = "Vive";
+	$str3 = "!";
+	echo $str2."LaPlateforme".$str3." " ;
+	$val = 6 ;
+	echo $val ;
+	$resultat = $val + 4 ;
+	echo "Le resultat est :".$resultat ;
+	$myBool = true ;
+	echo $myBool ;
+	$myBool = false ;
+	echo $myBool ;
+?>
