010fb6e64be2ea73f1045412022d9044b4517119
mohamed-azzouz <mohamed.azzouz@laplateforme.io>
2019-11-05 10:29:07 +0100

    Affichage certain nombre

---
+++ jour02/job02/index.php
@@ -0,0 +1,22 @@
+<?php
+    $nombre = 0;
+    $increment = +1;
+
+    while($nombre != 1338){
+            if($nombre == 26){
+            }
+            elseif($nombre == 37){
+            }
+            elseif($nombre == 88){
+            }
+            elseif($nombre == 1111){
+            }
+            elseif($nombre == 3233){
+            }
+            else{
+                echo $nombre;
+                echo "</br>";
+            }
+            $nombre = $nombre + $increment;
+        }
+?>
